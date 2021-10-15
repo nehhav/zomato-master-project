ad04340e4c63982a0f8bd1ede5c9d299efc2e72e
nehhav <nehhav@hotmail.com>
2021-10-15 19:19:09 +0530

    setup api

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// Importing Env Variables
+require("dotenv").config();
+
+//Libraries
+import express from "express"
+import cors from "cors";
+import helmet from "helmet";
+
+
+//microservice routes
+import Auth from "./API/Auth";
+
+//Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// application middlewares
+
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }))
+zomato.use(helmet());
+zomato.use(cors());
+
+//Application routes
+zomato.use("/auth", Auth);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success Yay!!!" }));
+
+zomato.listen(4000, () =>
+    ConnectDB()
+        .then(() => console.log("Server is running"))
+        .catch(() => console.log("DB Connection failed ")));
